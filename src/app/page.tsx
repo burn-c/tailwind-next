e2e21ff36a8b2f2cb622df7a615e061fb2a8d040
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-02 23:14:05 -0300

    feat(Home): responsiveness and breakpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function Home() {
   return (
     <div className="p-8 bg-slate-900 text-slate-100 h-screen">
-      <h1 className="font-bold text-5xl flex items-center gap-3 before:w-0.5 before:h-8 before:bg-sky-500">
+      <h1
+        className="
+        font-bold 
+        text-3xl sm:text-5xl lg:text-6xl 
+        flex items-center gap-3 
+        before:w-0.5 before:h-8 before:bg-sky-500"
+      >
         Hello Tailwind
       </h1>
       <h2 className="">Hello H2</h2>
