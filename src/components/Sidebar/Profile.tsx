f5fe6f433d29ebfe8ad0bbf84214a5617f02c11b
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-06 21:51:54 -0300

    feat(Sidebar): adds Profile

---
+++ src/components/Sidebar/Profile.tsx
@@ -0,0 +1,35 @@
+import { LogOut } from 'lucide-react'
+
+export function Profile() {
+  return (
+    <div className="grid-cols-profile grid items-center gap-3">
+      <img
+        src="https://github.com/burn-c.png"
+        className="h-10 w-10 rounded-full"
+        alt=""
+      />
+
+      <div className="flex flex-1  flex-col truncate">
+        <span
+          className="truncate text-sm font-semibold text-zinc-700"
+          title="Carlos Oliveira"
+        >
+          Carlos Oliveira
+        </span>
+        <span
+          className="truncate text-sm text-zinc-500"
+          title="emailmuitograndecarlos.burndev@gmail.com"
+        >
+          emailmuitograndecarlos.burndev@gmail.com
+        </span>
+      </div>
+
+      <button
+        type="button"
+        className="ml-auto rounded-md p-2 hover:bg-zinc-200"
+      >
+        <LogOut className="h-5 w-5 text-zinc-500" />
+      </button>
+    </div>
+  )
+}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,6 +13,7 @@ import {
 import Logo from './Logo'
 import { NavItem } from './NavItem'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
 
 export function Sidebar() {
   return (
@@ -45,6 +46,10 @@ export function Sidebar() {
         </nav>
 
         <UsedSpaceWidget />
+
+        <div className="h-px bg-zinc-200" />
+
+        <Profile />
       </div>
     </aside>
   )
