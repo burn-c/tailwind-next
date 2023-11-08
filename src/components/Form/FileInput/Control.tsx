787fa89430d5b0f4f0e0e42b4aa8f65493aea35b
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-08 16:09:10 -0300

    feat(FileInput): adds FileInputContext and useFileInput to share id, files and chages

--- src/components/Form/FileInput/Control.tsx
+++ src/components/Form/FileInput/Control.tsx
@@ -1,7 +1,33 @@
-import { ComponentProps } from 'react'
+'use client'
+
+import { ChangeEvent, ComponentProps } from 'react'
+import { useFileInput } from './Root'
 
 export type ControlProps = ComponentProps<'input'>
 
 export function Control(props: ControlProps) {
-  return <input type="file" id="photo" className="sr-only" {...props} />
+  const { id, onFilesSelected } = useFileInput()
+
+  function handleFilesSelected(event: ChangeEvent<HTMLInputElement>) {
+    const noFilesSelected = !event.target.files?.length
+
+    if (noFilesSelected) {
+      return
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const files = Array.from(event.target.files!)
+
+    onFilesSelected(files)
+  }
+
+  return (
+    <input
+      type="file"
+      id={id}
+      className="sr-only"
+      onChange={handleFilesSelected}
+      {...props}
+    />
+  )
 }
--- src/components/Form/FileInput/ImagePreview.tsx
+++ src/components/Form/FileInput/ImagePreview.tsx
@@ -1,9 +1,34 @@
+'use client'
+
 import { User } from 'lucide-react'
+import { useMemo } from 'react'
+
+import { useFileInput } from './Root'
 
 export function ImagePreview() {
-  return (
+  const { files } = useFileInput()
+
+  const previewURL = useMemo(() => {
+    const noHasFiles = files.length === 0
+    if (noHasFiles) {
+      return null
+    }
+
+    return URL.createObjectURL(files[0])
+  }, [files])
+
+  const nullImage = previewURL === null
+
+  return nullImage ? (
     <div className="flex h-16 w-16 items-center justify-center rounded-full bg-violet-50">
       <User className="h-8 w-8 text-violet-500" />
     </div>
+  ) : (
+    // eslint-disable-next-line @next/next/no-img-element
+    <img
+      src={previewURL}
+      alt=""
+      className="h-16 w-16 rounded-full object-cover"
+    />
   )
 }
