40455d83c5f3528f402469b30b8fc53a520a0096
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-08 15:13:10 -0300

    feat(Home): adds Avatar upload input

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,10 +4,20 @@ const config: Config = {
   content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      colors: {
+        violet: {
+          25: '#fcfaff',
+        },
+      },
+
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr max-content',
         form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
+      },
+
+      borderWidth: {
+        6: '6px',
       },
 
       maxWidth: {
