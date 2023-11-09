3abdb9de7b418179a1852e9fccd29bd8540450de
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-08 22:26:54 -0300

    refact(Select): adds SelectItem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { InputRoot, InputControl, InputPrefix } from '@/components/Input'
 import SettingTabs from '@/components/SettingsTabs'
 import { FileInput } from '@/components/Form/FileInput'
 import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -113,7 +114,10 @@ export default function Home() {
               Country
             </label>
 
-            <Select />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
@@ -123,7 +127,14 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div />
+
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC-08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
