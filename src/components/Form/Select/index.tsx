4f74a1add6836a08c485ea41e9489974eb9aa988
Carlos Oliveira <carlos.burndev@gmail.com>
2023-11-10 21:25:54 -0300

    style(Select): adds animation

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -27,7 +27,7 @@ export function Select({ children, placeholder, ...rest }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-width] overflow-hidden rounded-lg border border-zinc-200 bg-white shadow-sm"
         >
           <SelectPrimimitive.Viewport>{children}</SelectPrimimitive.Viewport>
         </SelectPrimimitive.Content>
